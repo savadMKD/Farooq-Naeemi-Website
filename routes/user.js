f572f16f1d0546d016fca3843f5875f87a1af96b
SavadMKD <savadmoorkkanad@gmail.com>
2021-01-09 20:33:14 +0530

    Designed Speeches Video Section

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/home', { title: 'Express' });
+  res.render('user/home');
+});
+
+router.get('/ramadan', (req, res) => {
+  res.render('user/ramadan');
 });
 
 module.exports = router;
